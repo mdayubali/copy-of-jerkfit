f395ea62a47af4628acfdbd9a9062cfa4cc7a4b5
Md Ayub Ali <mohammadayubali88@gmail.com>
2023-09-20 09:53:54 +0600

    Slider section added

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function(){
+    $('.block-qoute').slick({
+        accessibility:true,
+        arrows:false,
+        autoplay:true,
+        autoplaySpeed: 3000,
+        dots:true,
+        dotsClass: 'slick-dots',
+        mobileFirst: true,
+
+    });
+  });
